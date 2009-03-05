58dcd13ef7130475971cab34ea7ddf0b39ea3578
Robert Cabri <robertcabri@Cabri.(none)>
2009-03-05 22:28:25 +0100

    edited templates

--- templates/page/editpage.php
+++ templates/page/editpage.php
@@ -2,26 +2,36 @@
 			<fieldset>
 				<legend>New page</legend>
 				<form action="<?php echo WWW_URL; ?>/page/editpage/" name="newpage" method="post">
+					<?php if (count($aErrors) > 0) : ?>
+					<ul class="error">
+					<?php foreach ($aErrors as $sError) : ?>
+						<li><?php echo Lang::get($sError); ?></li>
+					<?php endforeach; ?>
+					</ul>
+					<?php endif; ?>
 					<table>
-						<thead></thead>
 						<tbody>
 							<tr>
 								<td>
 									<label for="pagename">Pagename: </label>
 								</td>
 								<td>
-									<input type="text" name="pagename" id="pagename" />.php
+									<input type="text" name="pagename" id="pagename" value="<?php echo $sPagename; ?>" />.php
 								</td>
 							</tr>
 							<tr>
 								<td>
-									<label for="template">Template: </label>
+									<label for="template_id">Template: </label>
 								</td>
 								<td>
-									<select name="template">
+									<select name="template_id">
 										<option value="0">Select template..</option>
 										<?php foreach ($aTemplates as $oTemplateFile) : ?>
+										<?php if ($oTemplateFile->getID() == $iTemplateID) :?>
+										<option value="<?php echo $oTemplateFile->getID(); ?>" selected="selected"><?php echo $oTemplateFile->getTitle(); ?></option>
+										<?php else: ?>
 										<option value="<?php echo $oTemplateFile->getID(); ?>"><?php echo $oTemplateFile->getTitle(); ?></option>
+										<?php endif; ?>
 										<?php endforeach; ?>
 									</select>
 									Template cannot be changed
@@ -32,7 +42,7 @@
 									<label for="publishtime">Publishtime: </label>
 								</td>
 								<td>
-									<input type="text" name="publishtime" id="publishtime" value="" />
+									<input type="text" name="publishtime" id="publishtime" value="<?php echo $sPublishtime; ?>" />
 								</td>
 							</tr>
 							<tr>
@@ -40,17 +50,48 @@
 									<label for="expiretime">Expiretime: </label>
 								</td>
 								<td>
-									<input type="text" name="expiretime" id="expiretime" value="" />
+									<input type="text" name="expiretime" id="expiretime" value="<?php echo $sExpiretime; ?>" />
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<label for="redirect">Redirect: </label>
+								</td>
+								<td>
+									<input type="text" name="redirect" id="redirect" value="<?php echo $sRedirect; ?>" />
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<label for="active">Active: </label>
+								</td>
+								<td>
+									<input type="checkbox" name="active" id="active" value="<?php echo $iActive; ?>" />
 								</td>
 							</tr>
 							<tr>
 								<td>&nbsp;</td>
 								<td>
-									<input type="submit" value="save" name="save" />
-									<input type="submit" value="cancel" name="cancel" />
+									<input type="submit" value="save" name="action" />
+									<input type="submit" value="cancel" name="action" />
 								</td>
 							</tr>
 						</tbody>
 					</table>
+					<input type="hidden" name="page_id" value="<?php echo $iPageID; ?>" />
 				</form>
 			</fieldset>
+			<?php if ($iPageID != 0) :?>
+			<fieldset>
+				<legend>Modules</legend>
+				<?php if (count($aModules) == 0) : ?>
+					No modules for this page
+				<?php else: ?>
+				<ul>
+				<?php foreach ($aModules as $aModule) :?>
+					<li><?php echo $aModule['id']; ?> (<?php echo $aModule['module']; ?>) <a href="#">Edit</a></li>
+				<?php endforeach; ?>
+				</ul>
+				<?php endif;?>
+			</fieldset>
+			<?php endif; ?>
--- templates/page/pageoverview.php
+++ templates/page/pageoverview.php
@@ -1,37 +1,21 @@
-			<table>
-				<thead>
-					<tr>
-						<td><input type="checkbox" name="selectall" /></td>
-						<td>Title</td>
-						<td>Active</td>
-						<td>Actions</td>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td><input type="checkbox" name="select[]" /></td>
-						<td>Welkom</td>
-						<td>True</td>
-						<td>
-							<a href="#">Edit</a>
-							<a href="#">Down</a>
-							<a href="#">Up</a>
-							<a href="#">Delete</a>
-						</td>
-					</tr>
-					<tr>
-						<td><input type="checkbox" name="select[]" /></td>
-						<td>About</td>
-						<td>True</td>
-						<td>
-							<a href="#">Edit</a>
-							<a href="#">Down</a>
-							<a href="#">Up</a>
-							<a href="#">Delete</a>
-						</td>
-					</tr>
-				</tbody>
-				<tfoot>
-					
-				</tfoot>
-			</table>
+			<?php if (isset($oBreadCrumb)) : ?>
+			<?php echo $oBreadCrumb->getContents(); ?>
+			<?php endif; ?>
+
+			<?php if (isset($aErrors) && count($aErrors) > 0) : ?>
+				<?php foreach ($aErrors as $sError) : ?>
+					<li><?php echo Lang::get($sError); ?></li>
+				<?php endforeach; ?>
+			<?php endif; ?>
+			
+			<form method="post" action="<?php echo WWW_URL.'/page/search/' ?>">
+			<?php if (isset($oSearch)) : ?>
+			<?php echo $oSearch->getContents(); ?>
+			<?php endif; ?>
+			</form>
+			
+			<form method="post" action="<?php echo WWW_URL.'/page/' ?>">
+			<?php if (isset($oOverview)) : ?>
+			<?php echo $oOverview->getContents(); ?>
+			<?php endif; ?>
+			</form>
