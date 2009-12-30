032c871d319428c036d506da27ef73da05abdf8a
Robert Cabri <robert@dicabrio.com>
2009-12-30 19:41:46 +0100

    templatetitle has special needs

---
+++ application/modules/template/templatetitle.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class TemplateTitle extends DomainText {
+
+	public function __construct($value=null) {
+
+		if (empty($value)) {
+			throw new InvalidArgumentException('nothing-set', 1);
+		}
+
+		if (!preg_match("/^[a-zA-Z0-9]+$/", $value)) {
+			throw new InvalidArgumentException('contains-spaces', 1);
+		}
+		
+		parent::__construct($value, null, 255);
+	}
+}
