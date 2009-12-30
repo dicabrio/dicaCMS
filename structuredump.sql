fa536460ef18380368320f79032fe46e8adeec2b
Robert Cabri <robert@dicabrio.com>
2009-12-30 21:41:53 +0100

    added new structure dump of the database

--- structuredump.sql
+++ structuredump.sql
@@ -3,17 +3,31 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generatie Tijd: 22 Dec 2009 om 21:00
+-- Generatie Tijd: 30 Dec 2009 om 21:40
 -- Server versie: 5.1.38
 -- PHP Versie: 5.3.0
-
-SET FOREIGN_KEY_CHECKS=0;
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
 -- Database: `dicabrio`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabel structuur voor tabel `folder`
+--
+
+CREATE TABLE IF NOT EXISTS `folder` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `associationtype` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `folder_id` int(11) unsigned NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -30,8 +44,7 @@ CREATE TABLE IF NOT EXISTS `page` (
   `created` datetime NOT NULL,
   `redirect` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `active` int(1) NOT NULL,
-  `parent_id` int(11) unsigned NOT NULL,
-  `isfolder` int(1) NOT NULL,
+  `folder_id` int(11) unsigned NOT NULL,
   `keywords` text COLLATE utf8_unicode_ci,
   `description` text COLLATE utf8_unicode_ci,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -49,6 +62,19 @@ CREATE TABLE IF NOT EXISTS `pagemodule` (
   `page_id` int(11) unsigned NOT NULL,
   `identifier` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabel structuur voor tabel `pagemodule_staticblock`
+--
+
+CREATE TABLE IF NOT EXISTS `pagemodule_staticblock` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pagemodule_id` int(11) unsigned NOT NULL,
+  `staticblock_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -81,18 +107,30 @@ CREATE TABLE IF NOT EXISTS `pagetext` (
 -- --------------------------------------------------------
 
 --
+-- Tabel structuur voor tabel `staticblock`
+--
+
+CREATE TABLE IF NOT EXISTS `staticblock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Tabel structuur voor tabel `templatefile`
 --
 
 CREATE TABLE IF NOT EXISTS `templatefile` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` text COLLATE utf8_unicode_ci NOT NULL,
-  `filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `path` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
   `created` datetime NOT NULL,
-  `parent_id` int(11) NOT NULL,
-  `isfolder` int(1) NOT NULL,
+  `folder_id` int(11) NOT NULL,
+  `source` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -130,5 +168,3 @@ CREATE TABLE IF NOT EXISTS `user` (
   `active` int(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-SET FOREIGN_KEY_CHECKS=1;
