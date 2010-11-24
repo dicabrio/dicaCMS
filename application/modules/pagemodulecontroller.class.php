3ef33d9ae3cf5c352da4d130a6862ddf7c1b52d6
Robert Cabri <robert@dicabrio.com>
2010-11-24 22:08:28 +0100

    request is given as parameter

--- application/modules/pagemodulecontroller.class.php
+++ application/modules/pagemodulecontroller.class.php
@@ -8,14 +8,13 @@ interface PageModuleController {
 	 * @param PageModule $oMod
 	 * @param Page $oPage
 	 * @param Request $request
-	 * @return void
 	 */
 	public function __construct(PageModule $oMod, Page $page, Request $request);
 	
 	/**
 	 * get contents for this module as a string or an object that has a __toString method implemented
 	 * 
-	 * @return string
+	 * @return View
 	 */
 	public function getContents();
 	
