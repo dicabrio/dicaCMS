6fd92c98ba344a103e25571855bb55489648ad7e
Robert Cabri <robert@dicabrio.com>
2011-01-11 11:43:10 +0100

    added cms url to default example config

--- application/etc/default.example/general.php
+++ application/etc/default.example/general.php
@@ -1,20 +1,19 @@
 <?php
 
 $config = array(
-
 	'dir' => array(
-		'www' => realpath(SYS_DIR.'/www'),
-		'templates' => realpath(APP_DIR.'/templates'),
-		'lang' => realpath(APP_DIR.'/lang'),
+		'www' => realpath(SYS_DIR . '/www'),
+		'templates' => realpath(APP_DIR . '/templates'),
+		'lang' => realpath(APP_DIR . '/lang'),
 	),
-
 	'url' => array(
-		'www' => 'http://'.DOMAIN,
-		'images' => 'http://'.DOMAIN.'/images',
-		'css' => 'http://'.DOMAIN.'/css',
-		'js' => 'http://'.DOMAIN.'/js',
+		'www' => 'http://' . DOMAIN,
+		'images' => 'http://' . DOMAIN . '/images',
+		'css' => 'http://' . DOMAIN . '/css',
+		'js' => 'http://' . DOMAIN . '/js',
 	),
-
+	'cmsurl' => array(
+		'www' => 'http://' . DOMAIN . '/cms',
+	),
 	'default_lang' => 'NL',
-	
 );
